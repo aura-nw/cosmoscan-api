e1b08c62aad7ce3739425bc9546ce649203d1e93
nam3t <namnh2711@gmail.com>
2021-12-30 17:42:27 +0700

    update migration

--- dao/clickhouse/migrations/005_delegator_rewards.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS delegator_rewards;
--- dao/clickhouse/migrations/015_account_txs.up.sql
+++
@@ -1,6 +0,0 @@
-create table account_txs
-(
-    atx_account FixedString(45),
-    atx_tx_hash FixedString(64)
-) ENGINE ReplacingMergeTree() ORDER BY (atx_account, atx_tx_hash);
-
